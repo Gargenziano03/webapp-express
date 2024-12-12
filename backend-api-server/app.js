7c52a2a879968d8a3db0a27c71e8becf7f003c86
Gargenziano03 <gargenziano03@gmail.com>
2024-12-12 15:52:46 +0100

    add middlewares

--- backend-api-server/app.js
+++ backend-api-server/app.js
@@ -1,23 +1,32 @@
-const express = require('express')
+const express = require('express');
 const server = express();
-const MoviesRouter = require('./routes/movies')
+const MoviesRouter = require('./routes/movies');
+const NotFound = require('./middlewares/NotFound')
+const ServerErrorsHandler = require('./middlewares/ServerErrorsHandler')
 const cors = require('cors')
+const corsOrigin = {
+    origin: process.env.WEB_APP_ORIGIN_API,
+    optionsSuccessStatus: 200
+}
+server.use(cors(corsOrigin));
+
+
 const HOST = process.env.HOST || 'http://localhost';
 const PORT = process.env.PORT || '3000';
 
-const corsOrigin = {
-    origin: process.env.ORIGIN_API,
-    optionsSuccessStatus: 200
-}
+server.get('/', (req, res) => {
+    res.send(`Server is up and running!`);
+})
 
 server.use('/api/movies', MoviesRouter);
 
+server.use(NotFound)
+
+server.use(ServerErrorsHandler)
+
 server.listen(PORT, () => {
     console.log(`Server is listenig on port ${HOST}:${PORT}`);
-
 })
 
-server.get('/', (req, res) => {
-    res.send(`server is up and running`);
-})
+
 
