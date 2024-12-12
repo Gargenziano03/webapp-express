d6af22230b1e15e49b96eb2aa20414e0a6b48827
Gargenziano03 <gargenziano03@gmail.com>
2024-12-12 11:38:58 +0100

    add cors

--- backend-api-server/app.js
+++ backend-api-server/app.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const server = express();
 const MoviesRouter = require('./routes/movies')
-
+const cors = require('cors')
 const HOST = process.env.HOST || 'http://localhost';
 const PORT = process.env.PORT || '3000';
+
+const corsOrigin = {
+    origin: process.env.ORIGIN_API,
+    optionsSuccessStatus: 200
+}
 
 server.use('/api/movies', MoviesRouter);
 
